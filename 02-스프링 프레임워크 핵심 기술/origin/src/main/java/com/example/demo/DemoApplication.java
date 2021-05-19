942a4fa1d6db1b3686d3b4a9c76b86a3833d6704
osj3474 <handong3474@gmail.com>
2021-05-19 20:14:27 +0900

    Update Environment

--- 02-스프링 프레임워크 핵심 기술/origin/src/main/java/com/example/demo/DemoApplication.java
+++ 02-스프링 프레임워크 핵심 기술/origin/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,7 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
-import other.OtherPack;
 
 @SpringBootApplication
 public class DemoApplication {
