fec840b947d640fbd0cb3947e4a99a2678639336
osj3474 <handong3474@gmail.com>
2021-05-22 10:15:59 +0900

    Update ResourceLoader

--- 02-스프링 프레임워크 핵심 기술/origin/src/main/java/com/example/demo/AppRunner.java
+++ 02-스프링 프레임워크 핵심 기술/origin/src/main/java/com/example/demo/AppRunner.java
@@ -3,22 +3,26 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Component
 @EnableAsync
 public class AppRunner implements ApplicationRunner {
-
     @Autowired
-    ApplicationEventPublisher publisher;
+    ResourceLoader loader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(Thread.currentThread());
-        publisher.publishEvent(new MyEvent(this, 100));
+        Resource resource = loader.getResource("classpath:test.txt");
+        if(resource.exists()){
+            String content = Files.readString(Path.of(resource.getURI()));
+            System.out.println(content);
+        }
     }
-
-
 }
