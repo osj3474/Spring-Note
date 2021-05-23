48a15dd45a578cc834500fd48deec69d2a0124a4
osj3474 <handong3474@gmail.com>
2021-05-23 21:42:50 +0900

    Update | SpEL

---
+++ 02-스프링 프레임워크 핵심 기술/webtest/src/main/java/com/example/webtest/Sample.java
@@ -0,0 +1,16 @@
+package com.example.webtest;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sample {
+    private int data = 200;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
